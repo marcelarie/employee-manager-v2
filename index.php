4068e2e3f36a54c1a7aa3df6f7919dda84132a4b
Marcel <googlillo@gmail.com>
2021-02-28 19:22:48 +0100

    Add getByParameters on abstract model

--- index.php
+++ index.php
@@ -1,25 +1,3 @@
 <?php
 
-$parameters = [
- 'key1' => 'value',
- 'key2' => 'value',
- 'key3' => 'value',
- 'key4' => 'value',
-  ];
-
-function getByParameters($parameters)
-{
-    function query($parameter, $key)
-    {
-        return 'WHERE \'' . $key . '\' = \'' . $parameter . '\'';
-    }
-    
-    $result = array_map('query', $parameters, array_keys($parameters));
-    echo 'SELECT * FROM employees '. implode(' AND ', $result);
-    echo '<br>';
-}
-
-getByParameters($parameters);
-
-echo '<br>';
 require 'app/router/router.php';
--- lib/Model.php
+++ lib/Model.php
@@ -2,11 +2,10 @@
 
 abstract class Model
 {
-    
     private $name;
     private $dataBaseTable;
 
-    function __construct($dataBaseTable)
+    public function __construct($dataBaseTable)
     {
         // $this->name = $name;
         $this->dataBaseTable = $dataBaseTable;
@@ -14,21 +13,47 @@ abstract class Model
 
     public function connect()
     {
-        return new PDO(DNS,USER,PASSWORD);
+        return new PDO(DNS, USER, PASSWORD);
     }
-    public function get(){
-        return $this->connect()->query("SELECT * FROM $this->dataBaseTable")->fetch();
+    public function get()
+    {
+        return $this->connect()->query(
+            "SELECT * FROM $this->dataBaseTable"
+        )->fetch();
     }
-    public function getById($id){
-        return $this->connect()->query("SELECT * FROM $this->dataBaseTable WHERE email='$id'")->fetch();
+    public function getById($id)
+    {
+        return $this->connect()->query(
+            "SELECT * FROM $this->dataBaseTable WHERE email='$id'"
+        )->fetch();
     }
-    public function getByParam($param){
-        return $this->connect()->query("SELECT * FROM $this->dataBaseTable WHERE $param[0]='$param[1]'")->fetch();
-    }
-    public function update(){
+    public function getByParameters($parameters)
+    {
+        function querySelectGenerator($parameter, $key)
+        {
+            return "WHERE '$key' = '$parameter'";
+        }
+    
+        $multipleParameters = implode(' AND ', array_map(
+            'querySelectGenerator',
+            $parameters,
+            array_keys($parameters)
+        ));
 
+        return $this->connect()->query(
+            "SELECT * FROM $this->dataBaseTable $multipleParameters"
+        )->fetch();
     }
-    public function delete(){
-
+    // public function getByParam($param)
+    // {
+    //     return $this->connect()->query(
+    //         "SELECT * FROM $this->dataBaseTable WHERE $param[0]='$param[1]'"
+    //     )->fetch();
+    // }
+    public function update()
+    {
+    }
+    public function delete()
+    {
     }
 }
